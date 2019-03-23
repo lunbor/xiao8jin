12390b67a7d1d0fe3a263ea1b7d5bc8bd012245d
zhouheng25520 <zhouheng.25520.sc@163.com>
2019-03-23 21:34:29 +0800

    提交main文件

---
+++ main.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import App from './App'
+
+import store from './store'
+
+import http from './http'
+
+Vue.config.productionTip = false
+
+Vue.prototype.$store = store
+
+Vue.prototype.$http = http;
+
+//Vue.prototype.websiteUrl = 'http://www.dolphin123.com'
+Vue.prototype.websiteUrl = 'http://www.guanjia.com'
+
+//Vue.prototype.shareUrl = 'http://h5.dolphin123.com'
+Vue.prototype.shareUrl = 'http://h5.guanjia.com'
+
+Vue.prototype.webshareTitle = '小8金管家-信用卡一族至爱。资金即时结算，代还、周转必备APP。'
+
+App.mpType = 'app'
+
+const app = new Vue({
+	store,
+    ...App
+})
+app.$mount()
